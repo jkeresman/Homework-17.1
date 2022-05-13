c7bcf2a8c5471600395c425ded64924f3fb95f8c
Josip Keresman <josipkeresman@gmail.com>
2022-05-13 21:26:47 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, make_response, render_template
+import random
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+
+    if request.method == "GET":
+
+        response = make_response(render_template("index.html"))
+
+        if not request.cookies.get("secret_number"):
+            response.set_cookie("secret_number", str(random.randint(1, 30)))
+
+        return response
+
+    elif request.method == "POST":
+
+        player_guess = request.form.get("player-guess")
+        secret_number = request.cookies.get("secret_number")
+
+        response = make_response(render_template("play_again.html",
+                                                 player_guess=player_guess,
+                                                 secret_number=secret_number,
+                                                 ))
+        print(secret_number)
+
+        if player_guess == secret_number:
+            response.set_cookie("secret_number", str(random.randint(1, 30)))
+
+        return response
+
+
+if __name__ == "__main__":
+    app.run(use_reloader=True)
